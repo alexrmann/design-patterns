f34dd4a1a52094c0039d6767a5c28bfeefdd09fb
Alex R. Mann <33494521+alexrmann@users.noreply.github.com>
2025-11-10 14:48:02 -0800

    Add preferred light-dark switch to component library

---
+++ components/switches/light-dark/light-dark.js
@@ -0,0 +1,24 @@
+// Requires GDPR compliance
+
+const html = document.querySelector("html");
+const mode = localStorage.getItem("mode");
+
+if (mode === "light") switchLight();
+if (mode === "dark") switchDark();
+
+// three functions used by buttons and radios
+
+function switchAuto() {
+  html.style.setProperty("color-scheme", "light dark");
+  localStorage.removeItem("mode");
+}
+
+function switchLight() {
+  html.style.setProperty("color-scheme", "light");
+  localStorage.setItem("mode", "light");
+}
+
+function switchDark() {
+  html.style.setProperty("color-scheme", "dark");
+  localStorage.setItem("mode", "dark");
+}
