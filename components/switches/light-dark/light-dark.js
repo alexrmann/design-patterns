c30b869cf43b6a3639b3c851b322f3a1c8b11151
Alex R. Mann <33494521+alexrmann@users.noreply.github.com>
2025-11-10 15:30:40 -0800

    Add a linked page to demonstrate local storage functionality for light-dark mode; Add h1 stylel; Check for all input modes and apply checked attribute to active mode; Add console messages

--- components/switches/light-dark/light-dark.js
+++ components/switches/light-dark/light-dark.js
@@ -3,6 +3,7 @@
 const html = document.querySelector("html");
 const mode = localStorage.getItem("mode");
 
+if (mode === null) switchAuto();
 if (mode === "light") switchLight();
 if (mode === "dark") switchDark();
 
@@ -10,15 +11,23 @@ if (mode === "dark") switchDark();
 
 function switchAuto() {
   html.style.setProperty("color-scheme", "light dark");
+  document.getElementById("auto").checked = true;
   localStorage.removeItem("mode");
+  console.log("Operating system preferred color scheme active");
+
 }
 
 function switchLight() {
   html.style.setProperty("color-scheme", "light");
+  document.getElementById("light").checked = true;
   localStorage.setItem("mode", "light");
+  console.log("Light mode active");
 }
+
 
 function switchDark() {
   html.style.setProperty("color-scheme", "dark");
+  document.getElementById("dark").checked = true;
   localStorage.setItem("mode", "dark");
+  console.log("Dark mode active");
 }
